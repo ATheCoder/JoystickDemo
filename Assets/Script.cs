ba78275215415f3b080f57cad3da5c15fafd0f6b
TheGreatA <nervous.fiend@gmail.com>
2017-06-24 16:56:51 +0430

    Instant Rotation

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -12,6 +12,8 @@ public class Script : MonoBehaviour {
     public float DamageMultiplier = 4f;
     public float DamageTolerance = 4f;
 
+    private Vector2 lookPos;
+
 	// Use this for initialization
 	void Start () {
         rb2D = this.GetComponent<Rigidbody2D>();
@@ -20,13 +22,19 @@ public class Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        rb2D.AddForce(new Vector2(0, CrossPlatformInputManager.GetAxis("Accelator") * 10));
+        rb2D.AddForce(new Vector2(0, CrossPlatformInputManager.GetAxis("Vertical") * 10));
         rb2D.AddForce(new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * 10 , 0));
         if(CrossPlatformInputManager.GetAxis("Horizontal") > 0) {
             this.transform.localScale = new Vector3(-1, 1, 1);
         }
         else if(CrossPlatformInputManager.GetAxis("Horizontal") < 0) {
             this.transform.localScale = new Vector3(1, 1, 1);
+        }
+        //Rotation Scripts:
+        if(CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0) {
+            lookPos = new Vector2(transform.position.x + CrossPlatformInputManager.GetAxis("Horizontal"), transform.position.y + CrossPlatformInputManager.GetAxis("Vertical"));
+            Vector2 Pos = new Vector2(transform.position.x, transform.position.y);
+            gameObject.transform.right = Pos - lookPos;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision) {
