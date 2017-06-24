d84eac3dca339fc3c9575b8b466496319754418b
TheGreatA <nervous.fiend@gmail.com>
2017-06-24 16:15:06 +0430

    Added HP Value and Text

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Script : MonoBehaviour {
     Rigidbody2D rb2D;
+    public Text hpValue;
     float health = 100f;
 	// Use this for initialization
 	void Start () {
         rb2D = this.GetComponent<Rigidbody2D>();
+        hpValue.text = health.ToString();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        rb2D.AddForce(new Vector2(0, CrossPlatformInputManager.GetAxis("Accelator") * 4));
-        rb2D.AddForce(new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * 4 , 0));
+        rb2D.AddForce(new Vector2(0, CrossPlatformInputManager.GetAxis("Accelator") * 10));
+        rb2D.AddForce(new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * 10 , 0));
         if(CrossPlatformInputManager.GetAxis("Horizontal") > 0) {
             this.transform.localScale = new Vector3(-1, 1, 1);
         }
@@ -22,13 +25,10 @@ public class Script : MonoBehaviour {
             this.transform.localScale = new Vector3(1, 1, 1);
         }
     }
-    private void OnCollisionStay2D(Collision2D collision) {
-        if(collision.gameObject.tag == "Solid") {
-            health -= 5;
-        }
-        if(health < 1) {
-            Die();
-        }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(rb2D.velocity.magnitude * rb2D.mass);
+        health -= Mathf.Ceil(rb2D.velocity.magnitude * rb2D.mass * 2);
+        hpValue.text = health.ToString();
     }
     void Die() {
         Debug.Log("I'm Dead!");
